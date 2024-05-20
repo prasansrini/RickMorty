df27a5e1fa38a07463adffead6b33c760d933843
Prasanna <github@prasansrini.com>
2024-05-20 20:27:11 +0200

    Added network module and integration.

--- app/src/main/java/com/rick/morty/feature_ram/presentation/MainActivity.kt
+++ app/src/main/java/com/rick/morty/feature_ram/presentation/MainActivity.kt
@@ -6,12 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rick.morty.feature_ram.ui.theme.RickAndMortyAppTheme
+import com.rick.network.components.TextComponent
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -21,29 +20,13 @@ class MainActivity : ComponentActivity() {
 		enableEdgeToEdge()
 		setContent {
 			RickAndMortyAppTheme {
-				Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-					Greeting(
+				Surface(modifier = Modifier.fillMaxSize()) {
+					TextComponent(
 						name = "Android",
-						modifier = Modifier.padding(innerPadding)
+						modifier = Modifier.padding(8.dp)
 					)
 				}
 			}
 		}
-	}
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-	Text(
-		text = "Hello $name!",
-		modifier = modifier
-	)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-	RickAndMortyAppTheme {
-		Greeting("Android")
 	}
 }
---
+++ network/src/main/java/com/rick/network/components/TextComponent.kt
@@ -0,0 +1,13 @@
+package com.rick.network.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TextComponent(name: String, modifier: Modifier) {
+	Text(
+		text = "Hello $name!",
+		modifier = modifier
+	)
+}
