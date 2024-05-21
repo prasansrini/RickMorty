d57d470312c41ae48f0edd724935386ad41092a9
Prasanna <github@prasansrini.com>
2024-05-21 15:01:03 +0200

    Added episodes button.

--- mobile/app/src/main/java/com/rick/morty/screens/CharacterDetailsScreen.kt
+++ mobile/app/src/main/java/com/rick/morty/screens/CharacterDetailsScreen.kt
@@ -1,5 +1,7 @@
 package com.rick.morty.screens
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
@@ -10,6 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
@@ -19,12 +22,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.SubcomposeAsyncImage
 import com.rick.morty.feature_ram.components.character.CharacterDetailsNamePlateComponent
 import com.rick.morty.feature_ram.components.common.DataPoint
 import com.rick.morty.feature_ram.components.common.DataPointComponent
 import com.rick.morty.feature_ram.components.common.LoadingState
+import com.rick.morty.feature_ram.ui.theme.RickAction
 import com.rick.network.models.domain.Character
 import com.rick.network.repository.RickMortyApiClient
 import kotlinx.coroutines.delay
@@ -145,8 +151,29 @@ fun CharacterDetailsScreen(
 			DataPointComponent(dataPoint = dataPoint)
 		}
 
+		item { Spacer(modifier = Modifier.height(32.dp)) }
+
+		// Episodes button
 		item {
-			Spacer(modifier = Modifier.height(8.dp))
+			Text(text = "View all episodes",
+				color = RickAction,
+				fontSize = 18.sp,
+				textAlign = TextAlign.Center,
+				modifier = Modifier
+					.padding(horizontal = 32.dp)
+					.border(
+						width = 1.dp,
+						color = RickAction,
+						shape = RoundedCornerShape(12.dp)
+					)
+					.clip(RoundedCornerShape(12.dp))
+					.clickable {
+
+					}
+					.padding(vertical = 8.dp)
+					.fillParentMaxWidth())
 		}
+
+		item { Spacer(modifier = Modifier.height(32.dp)) }
 	}
 }
